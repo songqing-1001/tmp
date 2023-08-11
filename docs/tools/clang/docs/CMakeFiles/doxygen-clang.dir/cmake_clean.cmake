8fc92ea17d2c44051d6e90dff0efc0df4131067a
cambricon <songqing@126.com>
2023-08-11 18:51:29 +0800

    backup

---
+++ docs/tools/clang/docs/CMakeFiles/doxygen-clang.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doxygen-clang"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/doxygen-clang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ docs/tools/mlir/docs/CMakeFiles/doxygen-mlir.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doxygen-mlir"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/doxygen-mlir.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
